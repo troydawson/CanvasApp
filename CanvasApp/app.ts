4bc31fdd732f6213efe35d696a6d1f5ce3611550
troydawson <troy-dawson@comcast.net>
2013-02-02 14:59:58 -0800

    added underscore and box2d

--- CanvasApp/app.ts
+++ CanvasApp/app.ts
@@ -1,51 +1,113 @@
+﻿/// <reference path="defs/underscore-typed.d.ts" />
+/// <reference path="defs/box2dweb.d.ts" />
 /// <reference path="defs/easeljs.d.ts" />
 /// <reference path="defs/toastr.d.ts" />
 
-var stage: createjs.Stage;
+var box2d = {
+	b2Vec2: Box2D.Common.Math.b2Vec2,
+	b2BodyDef: Box2D.Dynamics.b2BodyDef,
+	b2Body: Box2D.Dynamics.b2Body,
+	b2FixtureDef: Box2D.Dynamics.b2FixtureDef,
+	b2Fixture: Box2D.Dynamics.b2Fixture,
+	b2World: Box2D.Dynamics.b2World,
+	b2MassData: Box2D.Collision.Shapes.b2MassData,
+	b2PolygonShape: Box2D.Collision.Shapes.b2PolygonShape,
+	b2CircleShape: Box2D.Collision.Shapes.b2CircleShape,
+	b2DebugDraw: Box2D.Dynamics.b2DebugDraw
+};
 
-class Board {
+var SCALE = 30;
+var stage, world;
+var GROUND_W = 800;
+var GROUND_H = 20;
+var CIRCLE_RADIUS = 32;
 
-	constructor(name: string)
-	{
-		stage = new createjs.Stage(<HTMLCanvasElement> document.getElementById("canvas"));
+function setupPhysics() {
+	world = new box2d.b2World(new box2d.b2Vec2(0, 100), true);
 
-		var back = new createjs.Shape();
-		back.graphics.beginFill("#FF0000").drawRect(0, 0, stage.canvas.width, stage.canvas.height);
-		stage.addChild(back);
+	// create ground shape
+	var shape = new createjs.Shape();
+	shape.graphics.beginFill("#F00").drawRect(0, 0, GROUND_W, GROUND_H);
+	shape.regX = GROUND_W / 2;
+	shape.regY = GROUND_H / 2;
+	stage.addChild(shape);
 
-		var txt = new createjs.Text("", "14px Joystix", "#FFF");
-		txt.text = "This text is rendered in canvas using the Text Object:\n\n";
-		txt.text += "The API is loosely based on Flash's display list, and should be easy to pick up for both JS and AS3 developers. The key classes are:\n\n";
-		txt.text += "DisplayObject\nAbstract base class for all display elements in Easel. Exposes all of the display properties (ex. x, y, rotation, scaleX, scaleY, alpha, shadow, etc) that are common to all display objects.\n\n"
-		txt.text += "Stage\nThe root level display container for display elements. Each time tick() is called on Stage, it will update and render the display list to its associated canvas.\n\n";
-		txt.text += "Container\nA nestable display container, which lets you aggregate display objects and manipulate them as a group.\n\n";
-		txt.text += "Text\nRenders text in the context of the display list."
-
-		txt.lineWidth = 800;
-		txt.textBaseline = "top";
-		txt.textAlign = "left";
-		txt.y = 50;
-		txt.x = 30;
-		stage.addChild(txt);
-
-		// use lineWidth and getMeasuredHeight to dynamically draw a background for our text:
-		var pad = 10;
-		var bg = new createjs.Shape();
-		bg.graphics.beginFill("#114").drawRect(txt.x - pad, txt.y - pad, txt.lineWidth + pad * 2, txt.getMeasuredHeight() + pad * 2);
-		stage.addChildAt(bg,1);
-
-		stage.update();
-	}
+	// create ground
+	var fixDef = new box2d.b2FixtureDef();
+	fixDef.density = 1;
+	fixDef.friction = 0.5;
+	var bodyDef = new box2d.b2BodyDef();
+	bodyDef.type = box2d.b2Body.b2_staticBody;
+	bodyDef.position.x = GROUND_W / 2 / SCALE;
+	bodyDef.position.y = (stage.canvas.height - GROUND_H) / SCALE;
+	bodyDef.userData = shape;
+	fixDef.shape = new box2d.b2PolygonShape();
+	(<Box2D.Collision.Shapes.b2PolygonShape>fixDef.shape).SetAsBox(GROUND_W / 2 / SCALE, GROUND_H / 2 / SCALE);
+	world.CreateBody(bodyDef).CreateFixture(fixDef);
 }
 
 class App {
 
 	constructor() { }
 
+	 tick(): void {
+
+	 	world.Step(1 / 60, 10, 10);
+
+		// Box2Dの計算結果を描画に反映
+		var body = world.GetBodyList();
+		while (body) {
+			var obj = body.GetUserData();
+			if (obj) {
+				var position = body.GetPosition();
+				obj.x = position.x * SCALE;
+				obj.y = position.y * SCALE;
+				obj.rotation = body.GetAngle() * 180 / Math.PI;
+			}
+			body = body.GetNext();
+		}
+
+		stage.update();
+	 }
+
+	createShape() {
+		// create shape
+		var shape = new createjs.Bitmap("resources/ball.png");
+		shape.regX = CIRCLE_RADIUS / 2;
+		shape.regY = CIRCLE_RADIUS / 2;
+		stage.addChild(shape);
+
+		// create ground
+		var fixDef = new box2d.b2FixtureDef();
+		fixDef.density = 1;
+		fixDef.friction = 0.5;
+		fixDef.restitution = 0.5;
+		var bodyDef = new box2d.b2BodyDef();
+		bodyDef.type = box2d.b2Body.b2_dynamicBody;
+		bodyDef.position.x = Math.random() * GROUND_W / SCALE;
+		bodyDef.position.y = 0 / SCALE;
+		bodyDef.userData = shape;
+		fixDef.shape = new box2d.b2CircleShape(CIRCLE_RADIUS / 2 / SCALE);
+		world.CreateBody(bodyDef).CreateFixture(fixDef);
+	}
+
+	init(): void {
+		stage = new createjs.Stage(<HTMLCanvasElement> document.getElementById("canvas"));
+		//	createjs.Touch.enable(stage);
+
+		GROUND_W = stage.canvas.width;
+
+		setupPhysics();
+
+		stage.onMouseDown = this.createShape;
+
+		createjs.Ticker.addListener(this);
+		createjs.Ticker.setFPS(60);
+		createjs.Ticker.useRAF = true;
+	}
+
 	start(): void {
 
-		new Board("original");
-
-		toastr.success('Hello!');
+		this.init();
 	}
 }
